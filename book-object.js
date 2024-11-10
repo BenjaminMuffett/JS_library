0314824b06f29de1a1979cbbac89cf87087da6d2
Benjamin Muffett <136661576+BenjaminMuffett@users.noreply.github.com>
2024-11-10 10:01:43 +0900

    display book on page

--- book-object.js
+++ book-object.js
@@ -5,9 +5,9 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
-    }
+    // this.info = function() {
+    //     console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
+    // }
 }
 
 const bookOne = new Book('How to cat', 'Sir Angel', 12, 'read');
@@ -18,4 +18,32 @@ function addBookToLibrary(title, author, pages, read) {
     // takes arguments, creates a book from those arguments, store new book object into an array.
     let createdBook = new Book(title, author, pages, read);
     myLibrary.push(createdBook);
+    console.log('Book added to library.')
 }
+
+// keep display logic separate from storage array logic 
+
+// function that loops through myLibrary and displays each book on page table/card 
+function displayBookToLibrary() {
+    const container = document.querySelector('.grid-container');
+    myLibrary.forEach((book) => {
+        const entry = document.createElement('div');
+        entry.classList.add('card');
+        for (key in book) {
+            const text = document.createElement('p');
+            text.textContent = book[key];
+            entry.appendChild(text);
+        }
+        container.appendChild(entry);
+    })
+}
+
+
+
+// for (let book in myLibrary) {
+//     const entry = document.createElement('div');
+//     entry.classList.add('card');
+//     entry.textContent = book[0];
+//     console.log(book)
+//     container.appendChild(entry);
+// }
