4342d23a04cd68c6ff93270ea5fb79694359599e
Benjamin Muffett <136661576+BenjaminMuffett@users.noreply.github.com>
2024-11-11 10:54:41 +0900

    modal pop up, library adding logic modified

--- book-object.js
+++ book-object.js
@@ -17,13 +17,17 @@ const bookThree = new Book('Reasons to skip leg day', 'Redditor', 69, 'not read'
 function addBookToLibrary(title, author, pages, read) {
     // takes arguments, creates a book from those arguments, store new book object into an array.
     let createdBook = new Book(title, author, pages, read);
-    myLibrary.push(createdBook);
-    console.log('Book added to library.')
+    if (myLibrary.find(o => o.title == createdBook.title) == undefined) {
+        myLibrary.push(createdBook);
+        console.log('Book added to library.')
+    }
+    
 }
 
 // keep display logic separate from storage array logic 
 
 // function that loops through myLibrary and displays each book on page table/card 
+// rather than for each in the array, just append the last entry 
 function displayBookToLibrary() {
     const container = document.querySelector('.grid-container');
     myLibrary.forEach((book) => {
@@ -38,12 +42,30 @@ function displayBookToLibrary() {
     })
 }
 
+// link button to a dialog, get info, create book entry
+
+const newBookButton = document.querySelector('#newBook');
+const bookDialog = document.querySelector('#bookDialog');
+const confirmButton = bookDialog.querySelector('#confirmBtn');
+const bookTitle = bookDialog.querySelector('#book_name');
+const bookAuthor = bookDialog.querySelector('#author');
+const bookPages = bookDialog.querySelector('#pages');
+const bookRead = bookDialog.querySelector('#read');
 
 
-// for (let book in myLibrary) {
-//     const entry = document.createElement('div');
-//     entry.classList.add('card');
-//     entry.textContent = book[0];
-//     console.log(book)
-//     container.appendChild(entry);
-// }
+newBookButton.addEventListener("click", () => {
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = ''; 
+    bookRead.value = '';
+    bookDialog.showModal();
+});
+
+confirmButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (bookTitle.value != '') {
+        const userBook = addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+        displayBookToLibrary(userBook);
+        bookDialog.close();
+    }
+});
