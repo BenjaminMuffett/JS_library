73f597f1c1a90cd4b3e658af5d8ab38e4f5d901b
Benjamin Muffett <136661576+BenjaminMuffett@users.noreply.github.com>
2024-11-26 09:15:28 +0900

    changed book into class rather than constructor

--- book-object.js
+++ book-object.js
@@ -10,13 +10,24 @@ function Book(title, author, pages, read) {
     // }
 }
 
+class ClassBook {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+
 const bookOne = new Book('How to cat', 'Sir Angel', 12, 'read');
 const bookTwo = new Book('Muscle building for dummies', 'Zyzz', 420, 'read');
 const bookThree = new Book('Reasons to skip leg day', 'Redditor', 69, 'not read');
 
 function addBookToLibrary(title, author, pages, read) {
     // takes arguments, creates a book from those arguments, store new book object into an array.
-    let createdBook = new Book(title, author, pages, read);
+    // let createdBook = new Book(title, author, pages, read);
+    let createdBook = new ClassBook(title, author, pages, read)
     myLibrary.push(createdBook);
     console.log('Book added into library.');
 }
